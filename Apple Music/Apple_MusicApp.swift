a2658a76a57655abe57ab2c8778d2f8fbb28228d
mrzogy <magbolmrzogy@gmail.com>
2023-05-19 16:52:03 +0300

    Start

---
+++ Apple Music/Apple_MusicApp.swift
@@ -0,0 +1,17 @@
+//
+//  Apple_MusicApp.swift
+//  Apple Music
+//
+//  Created by 3bood on 29/10/1444 AH.
+//
+
+import SwiftUI
+
+@main
+struct Apple_MusicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
